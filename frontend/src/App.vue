a1470e64da17f46d81c71cac17e3eb488e0d9739
“Mathieu” <mathieu.rio@epitech.eu>
2025-09-16 14:59:59 +0200

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<section>
+		<h2>Vue 3 + Django 🎉</h2>
+		<p>Ceci est rendu par Vue.</p>
+	</section>
+</template>
+
+<script setup>
+// rien pour le moment
+</script>
+
+<style scoped>
+	h2 {
+		margin: 1rem 0;
+	}
+</style>
---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+	plugins: [vue()],
+	server: {
+		host: 'localhost',
+		port: 5173
+	},
+	build: {
+		outDir: '../static/frontend',	// fichiers buildés -> Django static
+		emptyOutDir: true,
+		manifest: true,
+		rollupOptions: {
+			input: resolve(__dirname, 'src/main.js')
+		}
+	},
+	// Optionnel si tu comptes servir sous /static/
+	// base: '/static/'
+})
